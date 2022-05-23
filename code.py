06c7448473892a5b7fbc3652b495e57865622d49
krixgris <83347557+krixgris@users.noreply.github.com>
2022-05-23 20:43:09 +0200

    Add files via upload

--- code.py
+++ code.py
@@ -14,9 +14,23 @@ import random
 import grid_numbers
 from colors import COLORS
 from midi_notes import MIDI_NOTES
+import rgb_multiply
 
 
 MACROPAD_BRIGHTNESS = 0.15
+MACROPAD_SLEEP_KEYS = 10.0
+
+macropad_sleep_keys = False
+
+MACRO_PAD_DEFAULT_PAGE = "1"
+MODES = ["Transport","Volume"]
+
+class Display:
+	def __init__(self):
+		self.rows = ["",
+					"",
+					"",
+					""]
 
 #
 # Name change + move it out to separate py-file + rethink some functionality to be more general and also use for normal keypresses?
@@ -37,7 +51,7 @@ class MidiConfig:
 		return (f"({self.description}: cc={self.cc}, max={self.max_value}, min={self.min_value})")
 	def str(self):
 			return (f"({self.description}: cc={self.cc}, max={self.max_value}, min={self.min_value})")
-	def msg(self,value=None):
+	def msg(self,value=None, cc_offset=0):
 		if(value is None):
 			value = self.current_value
 		else:
@@ -49,7 +63,7 @@ class MidiConfig:
 		if(self.current_value<self.min_value):
 			self.current_value = self.min_value
 		return_value = self.current_value
-		return ControlChange(self.cc,return_value)
+		return ControlChange(self.cc+cc_offset,return_value)
 
 # depr. all of this should exist in the class containing all midi config
 class MacroPadMidi:
@@ -67,10 +81,33 @@ midi_keys = list()
 midi_encoder = None
 midi_encoder_click = None
 
+#midi_key_pages = dict()
+
 # depr
 def load_midi_config(midi_list, config_list):
 	for c in config_list:
 		midi_list.append(MidiConfig(c))
+
+def load_config(conf, midi_keys,midi_cc_lookup, page=MACRO_PAD_DEFAULT_PAGE):
+	# load json-configuration into appropriate objects
+	#midi_key_pages[page] = midi_keys
+	midi_keys.clear()
+	midi_cc_lookup.clear()
+	for k in range(0,12):
+		midi_keys.append(MidiConfig(conf['controller'][str(page)][str(k+1)]))
+	for k in midi_keys:
+		midi_cc_lookup[k.cc] = k.key
+	# print(f"{midi_keys=}")
+	# print(f"{midi_cc_lookup=}")
+
+def set_screen(macropad,text_lines,display_rows):
+	text_lines = macropad.display_text(display_rows.rows[0])
+	macropad.display_text(display_rows.rows[0])
+	for l in range(0,3):
+		text_lines[l].text = display_rows.rows[l+1]
+	text_lines.show()
+
+
 
 # these can and should all be replaced by the midi_keys, midi_encoder, midi_encoder_click
 pad_midi_values = MacroPadMidi()
@@ -80,18 +117,17 @@ conf_file = open(MIDI_CONFIG_JSON)
 conf = json.load(conf_file)
 conf_file.close()
 
-# load json-configuration into appropriate objects
-for k in range(0,12):
-	midi_keys.append(MidiConfig(conf['controller']['1'][str(k+1)]))
-midi_encoder = MidiConfig(conf['controller']['1']['enc'])
-midi_encoder_click = MidiConfig(conf['controller']['1']['enc_click'])
+
+
+midi_encoder = MidiConfig(conf['controller'][MACRO_PAD_DEFAULT_PAGE]['enc'])
+midi_encoder_click = MidiConfig(conf['controller'][MACRO_PAD_DEFAULT_PAGE]['enc_click'])
 
 # create lookup dictionary for looking up associated key with cc, mainly for lighting keys
 # can probably be done smarter, but this is convenient
 
 midi_cc_lookup = dict()
-for k in midi_keys:
-	midi_cc_lookup[k.cc] = k.key
+
+load_config(conf,midi_keys,midi_cc_lookup,MACRO_PAD_DEFAULT_PAGE)
 
 # depr. used to keep track of how long time things take, initial idea was to build a task list to run asynchronous
 start_time = time.monotonic()
@@ -104,6 +140,9 @@ macropad.display.auto_refresh = False
 key_color = colorwheel(130)  # fill with cyan to start
 
 macropad.pixels.brightness = MACROPAD_BRIGHTNESS
+
+
+macropad_mode = int(MACRO_PAD_DEFAULT_PAGE)
 
 #depr
 # --- MIDI variables ---
@@ -133,10 +172,22 @@ key_numbers = [
             9, 10, 11
             ]
 
+blank_display = Display()
+display_rows = Display()
+display_rows.rows[0] = "Macropad Cubase"
+display_rows.rows[1] = f"{MODES[macropad_mode-1]}"
+display_rows.rows[2] = ""
+display_rows.rows[3] = "Last Row"
+
 # --- Display text setup ---
-text_lines = macropad.display_text("Macropad Cubase")
-text_lines[1].text = ""
+#text_lines = macropad.display_text(display_rows.rows[0])
+text_lines = macropad.display_text("")
+# macropad.display_text(display_rows.rows[0])
+# for l in range(0,3):
+# 	text_lines[l].text = display_rows.rows[l+1]
 text_lines.show()
+
+set_screen(macropad, text_lines, display_rows)
 
 #read init position, for deltas
 last_knob_pos = macropad.encoder  # store knob position state
@@ -147,6 +198,7 @@ sleep_time = 0.5
 # variables set up with the idea to build task list
 loop_start_time = time.monotonic()
 last_run_time = time.monotonic()
+loop_last_action = time.monotonic()
 
 # for fun, loops through numbers showing on the display and playing audio
 def loop_numbers(iterations,reverse:bool = 0):
@@ -162,12 +214,12 @@ def init_colors():
 	for k in midi_keys:
 		macropad.pixels[k.key] = k.off_color 
 
-
 init_colors()
 
 while True:
 	# keep track of time when loop started
 	loop_start_time = time.monotonic()
+	
 	#   Block for handling things asynchronous..kind of
 	#
 	# # condition to execute code if enough time has passed to simulate async instead of time.sleep
@@ -186,7 +238,8 @@ while True:
 	midi_event = macropad.midi.receive()
 	# if nothing is in the buffer, midi_event is always None
 	if midi_event is not None:
-
+		loop_last_action = time.monotonic()
+		#macropad_sleep_keys = False
 		# handle NoteOn
 		if isinstance(midi_event, NoteOn):
 			macropad.stop_tone()
@@ -204,6 +257,8 @@ while True:
 				pad_midi_values.encoder = midi_event.value
 				midi_encoder.current_value = midi_event.value
 				last_knob_pos = macropad.encoder
+				#macropad.pixels[0] = rgb_multiply.rgb_mult(0xFF00CF,midi_event.value*1.0/127.0)
+				
 			# Encoder click CC
 			if midi_event.control == midi_encoder_click.cc:
 				pad_midi_values.encoder_click = midi_event.value
@@ -214,7 +269,7 @@ while True:
 				pad_midi_values.keys[key] = midi_event.value
 				midi_keys[key].current_value = midi_event.value
 				key_on = True if midi_event.value > 0 else False
-				event_color = midi_keys[key].on_color
+				event_color = rgb_multiply.rgb_mult(midi_keys[key].on_color, midi_event.value*1.0/127.0)
 				event_color = event_color if key_on else midi_keys[key].off_color
 				k_color = event_color
 				macropad.pixels[key] = k_color
@@ -227,11 +282,14 @@ while True:
 	# START KEYPAD EVENT HANDLER
 	################################################################
 	while macropad.keys.events:  # check for key press or release
+		loop_last_action = time.monotonic()
+		#macropad_sleep_keys = False
 		key_event = macropad.keys.events.get()
 
 		if key_event:
 			if key_event.pressed:
 				key = key_event.key_number
+				# print(f"{midi_keys=}")
 				# macropad.midi.send(midi_keys[key].msg(pad_midi_values.keys[key]))
 				if(midi_keys[key].toggle == 0):
 					macropad.midi.send(midi_keys[key].msg(127))
@@ -254,15 +312,29 @@ while True:
 	################################################################
 	macropad.encoder_switch_debounced.update()  # check the knob switch for press or release
 	if macropad.encoder_switch_debounced.pressed:
-		macropad.midi.send(midi_encoder_click.msg(pad_midi_values.encoder_click))
+		loop_last_action = time.monotonic()
+		#macropad_sleep_keys = False
 		
+		macropad.midi.send(midi_encoder_click.msg(midi_encoder_click.current_value,cc_offset=macropad_mode-1))
+		macropad_mode = macropad_mode%len(MODES)+1
+
 		macropad.red_led = macropad.encoder_switch
-		text_lines[1].text = " "  # clear the note line
+		#macropad_mode = 1 if macropad_mode == 2 else 2
+		
+		#text_lines[1].text = f"{MODES[macropad_mode-1]}"
+		#text_lines.show()
+		display_rows.rows[1] = f"{MODES[macropad_mode-1]}"
+		set_screen(macropad,text_lines,display_rows)
+		load_config(conf,midi_keys,midi_cc_lookup,macropad_mode)
 
 	if macropad.encoder_switch_debounced.released:
+		loop_last_action = time.monotonic()
+		#macropad_sleep_keys = False
 		macropad.red_led = macropad.encoder_switch
 
 	if last_knob_pos is not macropad.encoder:  # knob has been turned
+		loop_last_action = time.monotonic()
+		#macropad_sleep_keys = False
 
 		knob_pos = macropad.encoder  # read encoder
 		knob_delta = knob_pos - last_knob_pos  # compute knob_delta since last read
@@ -290,5 +362,19 @@ while True:
 	################################################################
 	# END ENCODER EVENT HANDLER
 	################################################################
-
 	macropad.display.refresh()
+
+	# screen saver
+	if(loop_start_time-loop_last_action>MACROPAD_SLEEP_KEYS):
+		macropad.pixels.brightness = 0
+		macropad_sleep_keys = True
+		set_screen(macropad, text_lines, blank_display)
+	elif(macropad_sleep_keys and loop_start_time-loop_last_action<MACROPAD_SLEEP_KEYS):
+		macropad.pixels.brightness = MACROPAD_BRIGHTNESS
+		macropad_sleep_keys = False
+		set_screen(macropad, text_lines, display_rows)
+
+	
+################################################################
+# END MAIN LOOP
+################################################################
